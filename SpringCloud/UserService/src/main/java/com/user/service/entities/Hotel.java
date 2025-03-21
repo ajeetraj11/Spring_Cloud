a91723c2ec2e6b2c2ce56a4274cbc94b2b102d71
ajeetraj11 <ajitpandey110920@gmail.com>
2025-03-21 19:40:17 +0530

    [Ajeet Raj] Integrated RestTemplate to fetch user ratings from Rating Service and retrieve hotel details from Hotel Service.

---
+++ SpringCloud/UserService/src/main/java/com/user/service/entities/Hotel.java
@@ -0,0 +1,15 @@
+package com.user.service.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hotel {
+
+    private String hotelid;
+    private String name;
+    private String location;
+    private String about;
+}
--- SpringCloud/UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
+++ SpringCloud/UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
@@ -1,20 +1,34 @@
 package com.user.service.services.impl;
 
+import com.user.service.entities.Hotel;
+import com.user.service.entities.Rating;
 import com.user.service.entities.User;
 import com.user.service.exception.ResourceNotFountException;
 import com.user.service.repositories.UserRepository;
 import com.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
      @Autowired
      private UserRepository userRepository;
+
+     @Autowired
+     private RestTemplate restTemplate;
+
+     private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
      @Override
      public User saveUser(User user) {
@@ -25,11 +39,32 @@ public class UserServiceImpl implements UserService {
 
      @Override
      public List<User> getAllUser() {
+
           return userRepository.findAll();
      }
 
      @Override
      public User getUser(String userId) {
-          return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFountException("User with given id is not found on server !!" + userId));
+          User user = userRepository.findById(userId).orElseThrow(()-> new ResourceNotFountException("User with given id is not found on server !!" + userId));
+
+          Rating[] ratingOfUser = restTemplate.getForObject("http://localhost:8083/ratings/user/"+user.getUserId(), Rating[].class);
+          logger.info("{}", ratingOfUser);
+
+          List<Rating> ratings = Arrays.stream(ratingOfUser).toList();
+
+          List<Rating> ratingList = ratings.stream().map(rating -> {
+
+               ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+               Hotel hotel = forEntity.getBody();
+               logger.info("response status code: {}", forEntity.getStatusCode());
+
+               rating.setHotel(hotel);
+
+               return rating;
+          }).collect(Collectors.toList());
+
+          user.setRatings(ratingList);
+
+          return user;
      }
 }
